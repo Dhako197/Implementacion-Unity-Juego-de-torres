41d43779878d444a870d47d8c46c7f66e3360c77
Dhako197 <53204420+Dhako197@users.noreply.github.com>
2022-04-27 21:57:42 -0500

    Probando drag y drop
    Digamos que funciono, pero no fuera del canvas, este funciona para ui no para juego

---
+++ Juego De Torres/Assets/Scripts/DragHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin drag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       transform.position= Input.mousePosition;
+            
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
